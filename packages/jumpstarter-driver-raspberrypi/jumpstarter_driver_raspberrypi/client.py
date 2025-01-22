6e38ab34e566cae8ff139789e57b3f45f1692a1b
Nick Cao <nickcao@nichi.co>
2025-01-22 13:07:19 -0500

    rework raspberry pi driver with digitalio

--- packages/jumpstarter-driver-raspberrypi/jumpstarter_driver_raspberrypi/client.py
+++ packages/jumpstarter-driver-raspberrypi/jumpstarter_driver_raspberrypi/client.py
@@ -1,37 +1,55 @@
 from dataclasses import dataclass
 
-import click
-from jumpstarter_driver_power.client import PowerClient
+from digitalio import DriveMode, Pull
 
-from jumpstarter.client import DriverClient
-
-
-@dataclass(kw_only=True)
-class DigitalOutputClient(PowerClient):
-    def on(self):
-        """Turn power on"""
-        self.call("on")
-
-    def off(self):
-        """Turn power off"""
-        self.call("off")
-
-    def cli(self):
-        @click.group()
-        def gpio():
-            """GPIO power control commands"""
-            pass
-
-        for cmd in super().cli().commands.values():
-            gpio.add_command(cmd)
-
-        return gpio
+from jumpstarter.client import PowerClient
 
 
 @dataclass(kw_only=True)
-class DigitalInputClient(DriverClient):
-    def wait_for_active(self, timeout: float | None = None):
-        self.call("wait_for_active", timeout)
+class DigitalIOClient(PowerClient):
+    """DigitalIO (Digital GPIO) client class
 
-    def wait_for_inactive(self, timeout: float | None = None):
-        self.call("wait_for_inactive", timeout)
+    Client methods for the DigitalIO driver.
+    """
+
+    def switch_to_output(self, value: bool = False, drive_mode: DriveMode = DriveMode.PUSH_PULL) -> None:
+        """
+        Switch pin to output mode with given default value and drive mode
+        """
+
+        match drive_mode:
+            case DriveMode.PUSH_PULL:
+                drive_mode = 0
+            case DriveMode.OPEN_DRAIN:
+                drive_mode = 1
+            case _:
+                raise ValueError("unrecognized drive_mode")
+        self.call("switch_to_output", value, drive_mode)
+
+    def switch_to_input(self, pull: Pull | None = None) -> None:
+        """
+        Switch pin to input mode with given pull up/down mode
+        """
+
+        match pull:
+            case None:
+                pull = 0
+            case Pull.UP:
+                pull = 1
+            case Pull.DOWN:
+                pull = 2
+            case _:
+                raise ValueError("unrecognized pull")
+        self.call("switch_to_input", pull)
+
+    @property
+    def value(self) -> bool:
+        """
+        Current value of the pin
+        """
+
+        return self.call("get_value")
+
+    @value.setter
+    def value(self, value: bool) -> None:
+        self.call("set_value", value)
